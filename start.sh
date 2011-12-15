d0a83c849e76eff13bd94ce01d9156f1ae731551
Puneeth Chaganti <punchagan@gmail.com>
2011-12-15 12:24:11 +0530

    Improve start script for cron-job.

--- start.sh
+++ start.sh
@@ -1,6 +1,13 @@
-if [ `ps -ef|grep chatroom|grep -v grep|wc -l` -lt 1 ]
-then
-echo "Bot not running, Starting ..."
 cd /home/punchagan/childrens-park/
-/usr/bin/nohup ./vir-santa/bin/python chatroom.py &
+if [ `ps -ef|grep chatroom|grep -v grep|wc -l` -lt 1 ]; then
+    echo "Bot not running, Starting ..."
+    /usr/bin/nohup ./vir-santa/bin/python chatroom.py &
+elif [ `tail -1 nohup.out|grep -c -E "(WARNING - could not connect to server - aborting)|(Disconnected from server)"` -gt 0 ]; then
+    pid=`ps -ef|grep chatroom|head -1|tr -s " "|cut -d " " -f 2`
+    echo "killing bot:" $pid
+    kill -9 $pid
+    sleep 60
+    /usr/bin/nohup ./vir-santa/bin/python chatroom.py &
+else
+    echo "Bot running..."
 fi
